d396b2803ee353cb0e5f7f57b8613c3a5b5810f6
frankie <franklin.shaw@gmail.com>
2017-03-06 12:43:14 +0000

    date, transaction, deposit, withdraw

--- lib/account.rb
+++ lib/account.rb
@@ -2,10 +2,40 @@ class Account
 
   def initialize
     @balance = 0
+    @date = '01/01/2017'
+    @statement = []
+  end
+
+  def show_statement
+    return @statement
   end
 
   def show_balance
     return @balance
   end
+
+  def show_date
+    return @date
+  end
+
+  def change_date(new_date)
+    @date = new_date
+  end
+
+  def deposit(amount)
+    recipt = transaction(amount)
+    return "Today on #{@date}, you deposited £#{amount}, your new balance is £#{@balance}"
+  end
+
+  def withdraw(amount)
+    recipt = transaction(-amount)
+    return "Today on #{@date}, you withdrew £#{amount}, your new balance is £#{@balance}"
+  end
+
+  def transaction(amount)
+    @balance = @balance + amount
+    recipt = [@date, amount, @balance]
+  end
+
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,10 +2,50 @@ require './lib/account.rb'
 
 
 describe Account do
-  test_account = Account.new
 
-  it 'initializes with balance 0' do
-    expect(test_account.show_balance).to equal(0)
+  context "initializes values" do
+
+    it 'initializes with balance 0' do
+      expect(subject.show_balance).to eq(0)
+    end
+
+    it 'initializes with date 01/01/2017' do
+      expect(subject.show_date).to eq('01/01/2017')
+    end
+
+    it 'initializes with a statement' do
+      expect(subject.show_statement).to eq([])
+    end
+
   end
+
+  context "change_date" do
+    it 'can change the date' do
+      subject.change_date('02/01/2017')
+      expect(subject.show_date).to eq('02/01/2017')
+    end
+  end
+
+  context "transaction" do
+    it 'can transact 1 into account' do
+      expect(subject.transaction(1)).to eq(['01/01/2017', 1, 1])
+    end
+    it 'can transact -1 from account' do
+      expect(subject.transaction(-1)).to eq(['01/01/2017', -1, -1])
+    end
+  end
+
+  context "deposit" do
+    it 'can deposit 1 into account' do
+      expect(subject.deposit(1)).to eq("Today on 01/01/2017, you deposited £1, your new balance is £1")
+    end
+  end
+
+  context "withdraw" do
+    it 'can withdraw 1 from account' do
+      expect(subject.withdraw(1)).to eq("Today on 01/01/2017, you withdrew £1, your new balance is £-1")
+    end
+  end
+
 
 end
